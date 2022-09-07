9f6081426dde058b6cab592631a56bc2e8434015
Philip Kolarov <102211673+PhilipKolarov@users.noreply.github.com>
2022-09-07 10:44:33 +0300

    Update
    I have created an abstract class called Calculator, which will assign certain characteristics to the basic calculator

--- basic_calculator.py
+++ basic_calculator.py
@@ -1,58 +1,47 @@
-class Calculator:
-    VALUE_ERROR_MESSAGE = "Value must be an integer or a float"
-    SYMBOLS = "+-*/"
-    ZERO_DIVISION_MESSAGE = "Numbers cannot be divided by zero"
+from calculators.calculator import Calculator
+
+
+class BasicCalculator(Calculator):
 
     def __init__(self):
-        self.previous_result = 0
+        super().__init__()
 
     def set_first_num(self, first_num):
-        self.previous_result = first_num
+        self.latest_result = first_num
 
     def addition(self, second_num):
         self.validate_value_is_integer(second_num)
 
-        result = self.previous_result + second_num
+        result = self.latest_result + second_num
 
-        self.previous_result = result
-        return self.previous_result
+        self.latest_result = result
+        return self.latest_result
 
     def subtraction(self, second_num):
         self.validate_value_is_integer(second_num)
 
-        result = self.previous_result - second_num
+        result = self.latest_result - second_num
 
-        self.previous_result = result
-        return self.previous_result
+        self.latest_result = result
+        return self.latest_result
 
     def multiplication(self, second_num):
         self.validate_value_is_integer(second_num)
 
-        result = self.previous_result * second_num
+        result = self.latest_result * second_num
 
-        self.previous_result = result
-        return self.previous_result
+        self.latest_result = result
+        return self.latest_result
 
     def division(self, second_num):
         self.validate_value_is_integer(second_num)
         self.validate_value_is_not_zero(second_num)
 
-        result = self.previous_result / second_num
+        result = self.latest_result / second_num
         result_as_string = str(result)
 
-        self.previous_result = self.format_as_int_or_float(result_as_string)
-        return self.previous_result
-
-    def reset(self):
-        self.previous_result = 0
-
-    def validate_value_is_integer(self, value):
-        if not isinstance(value, int) and not isinstance(value, float):
-            raise ValueError(self.VALUE_ERROR_MESSAGE)
-
-    def validate_value_is_not_zero(self, second_num):
-        if second_num == 0:
-            raise ValueError(self.ZERO_DIVISION_MESSAGE)
+        self.latest_result = self.format_as_int_or_float(result_as_string)
+        return self.latest_result
 
     @staticmethod
     def format_as_int_or_float(result_as_string):
